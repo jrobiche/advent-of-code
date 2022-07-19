02fd327a1e691ef065dba6394e898761cad70ccd
jrobiche <33152499+jrobiche@users.noreply.github.com>
2022-07-18 22:06:26 -0500

    update 2021 day 05 file structure

--- 2021/day-05/test.py
+++ 2021/day-05/test.py
@@ -3,6 +3,6 @@
 from solution import solution_1, solution_2
 
 if __name__ == "__main__":
-    assert solution_1("example_input.txt") == 5
-    assert solution_2("example_input.txt") == 12
+    assert solution_1("test_input.txt") == 5
+    assert solution_2("test_input.txt") == 12
     print("All tests passed successfully")
